62cd2ab0637028e17b096d2bbeece2e353169871
Mato Lucic <mato.lucic@ericsson.com>
2022-04-25 13:13:42 +0200

    Completed module - fix

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,7 +5,7 @@ class QuizQuestion extends Component {
     return (
       <main>
         <section>
-          <p>QuizQuestion</p>
+          <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className="buttons">
           <ul>
